fd833ce49888129f6c201d2ed681cd9799f02d19
baxtik <baxtik88@gmail.com>
2023-06-04 09:22:57 +0500

    cinephile Navbar added

---
+++ src/components/UI/BtnMore.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-link to="/" class="more">
+    <img src="@/assets/images/more.svg" alt="">
+    <span class="more__text">Подробнее</span>
+  </router-link>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.more {
+  padding: 10px 15px;
+  background: #149A03;
+  border-radius: 10px;
+  display: flex;
+  gap: 15px;
+  justify-content: center;
+  align-items: center;
+  width: max-content;
+
+  &__text {
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 30px;
+    color: white;
+  }
+}
+</style>
